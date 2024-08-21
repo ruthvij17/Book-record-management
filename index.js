61d5c76c13299069dcd90a9bbf2d7512b5c8ff6b
ruthvij17 <130194046+ruthvij17@users.noreply.github.com>
2024-08-21 15:56:55 +0000

    Done with the day-2

--- index.js
+++ index.js
@@ -29,6 +29,113 @@ app.get("/users", (req, res) => {
   });
 });
 
+/**
+ * Route: /users/:id
+ * Method:GET
+ * Description: Getting a single user by their ID
+ * Access: Public
+ * Parameters: ID
+ */
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  //console.log(req.params);
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User does not exist",
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    message: "User Found",
+    data: user,
+  });
+});
+
+/**
+ * Route: /users/
+ * Method:POST
+ * Description: Creating a new user
+ * Access: Public
+ * Parameters: None
+ */
+
+app.post("/users", (req, res) => {
+  const { id, name, surname, email, subscriptionType, subscriptionDate } =
+    req.body;
+
+  const user = users.find((each) => each.id === id);
+
+  if (user) {
+    return res.status(404).json({
+      message: "User with the ID already exist",
+      success: false,
+    });
+  }
+  users.push({ id, name, surname, email, subscriptionType, subscriptionDate });
+  return res.status(201).json({
+    success: true,
+    message: "User added successfully",
+    data: users,
+  });
+});
+
+/**
+ * Route: /users/:id
+ * Method:PUT
+ * Description: Updating user by their ID
+ * Access: Public
+ * Parameters: ID
+ */
+
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not exist",
+    });
+  }
+  const updateUserData = users.map((each) => {
+    if (each.id === id) {
+      return {
+        ...each,
+        ...data,
+      };
+    }
+    return each;
+  });
+  return res.status(200).json({
+    success: true,
+    message: "User updated ",
+    data: updateUserData,
+  });
+});
+
+/**
+ * Route: /users/:id
+ * Method:PUT
+ * Description: Deleting user by their ID
+ * Access: Public
+ * Parameters: ID
+ */
+
+app.delete("/users/:id",(req,res)=>{
+  const { id } = req.params;
+  //const { data } = req.body;
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not exist",
+    });
+  }
+})
+
 app.get("*", (req, res) => {
   res.status(404).json({
     message: "This root does not exist",
